2f319523a4dcf6cdcc84803cdfbb9a0b81996f0a
propotolki <propotolki52@gmail.com>
2025-07-29 23:11:19 +0300

    Create Menu.jsx

---
+++ src/pages/Menu.jsx
@@ -0,0 +1,10 @@
+const Menu = ({ id }) => {
+  return (
+    <div id={id} className="p-4">
+      <h1 className="text-xl font-bold">Меню</h1>
+      <p>Здесь будет список напитков и еды.</p>
+    </div>
+  );
+};
+
+export default Menu;
