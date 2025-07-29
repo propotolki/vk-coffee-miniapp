13e0cb1e878c05a2bb606f73128334fcf21542be
propotolki <propotolki52@gmail.com>
2025-07-29 23:11:38 +0300

    Create Promos.jsx

---
+++ src/pages/Promos.jsx
@@ -0,0 +1,10 @@
+const Promos = ({ id }) => {
+  return (
+    <div id={id} className="p-4">
+      <h1 className="text-xl font-bold">Акции</h1>
+      <p>Список актуальных акций и скидок.</p>
+    </div>
+  );
+};
+
+export default Promos;
