13caee41e2f4429ec795dbcbbe0b1ef7c9bcc7f2
propotolki <propotolki52@gmail.com>
2025-07-30 01:49:26 +0300

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,1 @@
 export { transformVKBridgeAdaptivity } from './transformVKBridgeAdaptivity';
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { RouterProvider } from '@vkontakte/vk-mini-apps-router';
-import { App } from '../App'; // или без фигурных скобок, если default export
-import { router } from '../routes';
-
-const root = createRoot(document.getElementById('root'));
-
