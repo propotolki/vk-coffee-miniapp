185dffaa7d55d7d72ad42af0fc5483e643a610c2
propotolki <propotolki52@gmail.com>
2025-07-30 02:00:32 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from '@vkontakte/vk-mini-apps-router';
-import { App } from './App';
 import { router } from './routes';
 
 const root = createRoot(document.getElementById('root'));
-root.render(
-  <RouterProvider router={router}>
-    <App />
-  </RouterProvider>
-);
+root.render(<RouterProvider router={router} />);
